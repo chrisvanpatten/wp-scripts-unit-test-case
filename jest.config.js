074d4513f18c7e7c3d22e46b03641824efdcb9c3
Chris Van Patten <chris@vanpattenmedia.com>
2022-07-20 08:52:04 -0400

    try: Add custom Babel transformer

--- jest.config.js
+++ jest.config.js
@@ -3,4 +3,7 @@ const config = require("@wordpress/scripts/config/jest-unit.config.js");
 module.exports = {
   ...config,
   setupFilesAfterEnv: ["./jest.setup.js"],
+  transform: {
+    "^.+\\.[jt]sx?$": "<rootDir>/scripts/babel-transformer.js",
+  },
 };
